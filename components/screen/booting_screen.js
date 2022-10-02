e5845cfdd069c5d18566155c228f0a93acfede5f
Clouds-Of-Mana <113837689+Clouds-Of-Mana@users.noreply.github.com>
2022-10-02 03:38:38 -0500

    Update booting_screen.js

--- components/screen/booting_screen.js
+++ components/screen/booting_screen.js
@@ -12,7 +12,7 @@ function BootingScreen(props) {
             </div>
             <img width="200px" height="100px" className="md:w-1/5 w-1/2" src="./themes/Yaru/status/ubuntu_white_hex.svg" alt="Ubuntu Name" />
             <div className="text-white mb-4">
-                <a className="underline" href="https://www.linkedin.com/in/cloudsofmana/" rel="noreferrer noopener" target="_blank">linkedin</a>
+                <a className="underline" href="https://discord.gg/6WCpm9Bq8t" rel="noreferrer noopener" target="_blank">Discord</a>
                 <span className="font-bold mx-1">|</span>
                 <a href="mailto:support@isleoftheblessed.com" rel="noreferrer noopener" target="_blank" className="underline">Email</a>
             </div>
